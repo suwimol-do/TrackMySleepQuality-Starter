b1f7a1d2558ceadf3c59f9d4cf39204cfd0e58da
Suwimol <Suwimol.do@rmuti.ac.th>
2020-08-05 10:21:05 +0700

    6.2.6 Step 2: Add the click handler for the Start button

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerViewModel.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerViewModel.kt
@@ -62,5 +62,20 @@ class SleepTrackerViewModel(
 
 
     }
+    suspend fun onStartTracking() {
+        uiScope.launch {}
+        val newNight = SleepNight()
+        insert(newNight)
+        tonight.value = getTonightFromDatabase()
+
+
+    }
+
+    private suspend fun insert(Night: SleepNight) {
+        withContext(Dispatchers.IO) {
+            database.insert(Night)
+        }
+
+    }
 }
 
